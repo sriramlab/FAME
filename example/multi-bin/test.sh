a7395e2e9895d764b438320147f5ce9ccf7b12fb
Boyang Fu <boyang19@login4.hoffman2.idre.ucla.edu>
2023-10-20 14:55:54 -0700

    change example

--- example/multi-bin/test.sh
+++ example/multi-bin/test.sh
@@ -4,7 +4,7 @@
 gen=../small
 phen=../h2_0.5.pheno
 annot=multi-annot.txt
-../../build/GENIE_GxG -g $gen -p $phen -gxgbin 0 -snp 10  -k 10 -jn 10  -o small.multi.results -annot $annot 
+../../build/GENIE_GxG -g $gen -p $phen -gxgbin 1 -snp 10  -k 10 -jn 10  -o small.multi.results -annot $annot 
 
 
 
